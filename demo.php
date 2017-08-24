55e210c0fbef2c2bb600250a9c84c89c93888d25
nguyenviettoan95 <30643631+nguyenviettoan95@users.noreply.github.com>
2017-08-24 16:28:46 +0700

    them form dang nhap voi popup

--- demo.php
+++ demo.php
@@ -34,6 +34,49 @@
 						<p>Gọi mua hàng:<a herf="#"><span style="color: #ff0000;"><strong>1900.6480</strong></span></a></p>
 						<p>Tư vẫn sửa chữa:<a herf="#"><span style="color: #ff0000;"><strong>1900.636.011</strong></span></p>
 					</div>
+					<!--Tạo NÚt Đăng nhập-->
+					<button type="button" class="btn btn-info" data-toggle="modal" data-target="#dangnhap">Đăng Nhập </button>
+					<div class="modal fade" id="dangnhap" role="dialog">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<p style="text-align: center;">Đăng Nhập</p>
+								</div>
+								<div class="modal-body">
+									<form  class="form-horizontal" action="#" method="get" accept-charset="utf-8">
+										<div class="form-group">
+									      <label class="control-label col-sm-2" for="email">Email:</label>
+									      <div class="col-sm-10">
+									        <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+									      </div>
+									    </div>
+									    <div class="form-group">
+									      <label class="control-label col-sm-2" for="pwd">Password:</label>
+									      <div class="col-sm-10">          
+									        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+									      </div>
+									    </div>
+									    <div class="form-group">        
+									      <div class="col-sm-offset-2 col-sm-10">
+									        <div class="checkbox">
+									          <label><input type="checkbox" name="remember"> Remember me</label>
+									        </div>
+									      </div>
+									    </div>
+									    <div class="form-group">        
+									      <div class="col-sm-offset-2 col-sm-10">
+									        <button type="submit" class="btn btn-default">Đăng nhập</button>
+									      </div>
+									    </div>
+									</form>
+								</div>
+								<div class="modal-footer">
+									 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								</div>
+							</div>
+						</div>
+					</div>
+
 			</div>
 		</div>
 	</div><!--End header-->
